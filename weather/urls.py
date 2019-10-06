88ac05b4e6a6b75426f42bcd91926ca816f5ca28
suhailmalik07 <suhailmalik8057@gmail.com>
2019-10-07 01:15:51 +0530

    initial release

---
+++ weather/urls.py
@@ -0,0 +1,8 @@
+"""weather URL Configuration"""
+from django.urls import path
+from .views import home, about
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+]
